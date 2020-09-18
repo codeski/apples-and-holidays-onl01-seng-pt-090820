8d0bfd4afcf93f56cd3b6bd541c6263d36fbcb82
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-18 20:35:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,6 +89,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     return holidays.map do |holiday, supplies|
       if supplies.include?("BBQ")
+      return holiday
       end
     end
   end
