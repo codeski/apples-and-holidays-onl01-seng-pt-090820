cd713bf287707f572bf1b3f5552a68387b6ac7b9
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-18 19:13:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     holiday.each do |what, array|
-    puts season.to_s.capitalize! 
+    puts season.to_s.capitalize! + ":"
     puts holiday.to_s.capitalize!
 binding.pry
     end
