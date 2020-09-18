2c3db4c25bb444a1b145e621dee514f23600cf7e
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-18 18:31:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,9 +74,9 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts #{season} #{holiday} 
-    binding.pry
+    
   end
-  
+  binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
