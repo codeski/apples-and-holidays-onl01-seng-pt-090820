610be3290fa493afb6b96bd607f6e5bb52350b8e
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-18 20:18:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.each do |season, holiday|
+  holiday_hash.map do |season, holiday|
     holiday.map do |value, array|
       if array.include?("BBQ")
         return value
